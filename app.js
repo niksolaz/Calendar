54c9f29ce30ced5b4361254c8439f790e13c8b07
niksolaz <solazzo.nicola@gmail.com>
2016-03-23 18:22:38 +0100

    change Home

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(compression());
 app.use(express.static(`${__dirname}/public`));
-//app.use('/', require('./src/index'));
+app.use('/', require('./src/index'));
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
